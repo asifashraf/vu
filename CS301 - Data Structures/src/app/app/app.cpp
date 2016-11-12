90a5349d739685042054fa46aa75a4fed454d323
asifashraf <asif.log@gmail.com>
2016-11-12 11:17:42 +0500

    moved code in folders

---
+++ CS301 - Data Structures/src/app/app/app.cpp
@@ -0,0 +1,35 @@
+// app.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	string msg = "--- simple array iteration ---";
+
+	int x[6];
+
+	int i;
+
+	cout << msg << endl;
+
+	for (i = 0; i < 6; i++)
+	{
+		x[i] = 2 * i;
+		
+		cout << "Hello World " << i << endl;
+		  
+
+	}
+
+
+
+
+	system("PAUSE");
+
+	return EXIT_SUCCESS;
+}
+
+
